41bf26a8d475c6d970dd172e7e3831149a1de47d
Hui Ren <huiren16@gmail.com>
2022-07-03 09:46:24 -0500

    Initial commit

---
+++ infra/ec2.tf
@@ -0,0 +1,19 @@
+#  module "project_ec2" {
+#    source             = "./modules/ec2"
+#    name               = local.name
+#    account            = data.aws_caller_identity.current.account_id
+#    aws_ami            = data.aws_ami.amazon_linux_2.id
+#    private_subnet_ids = module.vpc.private_subnet_ids
+#    vpc_id             = module.vpc.vpc_id
+#  }
+
+  module "project_ec2" {
+   source             = "./modules/ec2"
+   instance_count     = var.instance_count
+   name               = local.name
+   account            = data.aws_caller_identity.current.account_id
+   aws_ami            = "ami-0ef8c7099ef697f3e"
+   private_subnet_ids = module.vpc.private_subnet_ids
+   public_subnet_ids  = module.vpc.public_subnet_ids
+   vpc_id             = module.vpc.vpc_id
+ }
